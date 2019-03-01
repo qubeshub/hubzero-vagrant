a7aaca0ef2b60ef3e1737a2bfd1ef2b4a140938e
M. Drew LaMar <mdlama@wm.edu>
2019-03-01 10:46:49 -0500

    [fix] Temporary fix for migration issues

--- provision/setup.sh
+++ provision/setup.sh
@@ -14,6 +14,7 @@ cp -r /var/www/app-scotch/config app/config
 cd /var/www/public/core
 php bin/composer install
 cd /var/www/public
+php muse migration --file=Migration20180126160144ComSearch.php -f
 php muse migration -i -f
 
 chown vagrant:vagrant /srv
